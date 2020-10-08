c7d8453885ca6cbf1ea09b6d888e6acd7b12bbdb
Tenma Endou <tenmaendou@MacBook-Air.local>
2020-10-08 23:43:33 +0900

    first commit

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class HelloController extends Controller
+{
+    public function index($id='zero') {
+        $data = [
+            'msg' => 'これはコントローラーから渡されたメッセージ',
+            'id' => $id,
+        ];
+        return view('hello.index', $data);
+    }
+
+}
