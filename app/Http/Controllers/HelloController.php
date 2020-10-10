1615ccba8904eb55dabaa78b1c93226bd6405d38
Tenma Endou <tenmaendou@MacBook-Air.local>
2020-10-10 21:19:08 +0900

    3-5 #2

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -8,12 +8,7 @@ use Illuminate\Http\Response;
 class HelloController extends Controller
 {
     public function index() {
-        $data = [
-            ['name'=>'Taro', 'mail'=>'taro@xxx'],
-            ['name'=>'Hanako', 'mail'=>'hanako@xxx'],
-            ['name'=>'sachiko', 'mail'=>'sachi@xxx'],
-        ];
-        return view('hello.index', ['data'=>$data]);
+        return view('hello.index', ['message'=>'Hello!']);
     }
 
     public function post(Request $request) {
