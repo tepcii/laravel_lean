a8fe54e13c96cdd41926dc27ccde31385fa0427c
Tenma Endou <tenmaendou@MacBook-Air.local>
2020-10-11 16:59:44 +0900

    4-3 #5

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -5,19 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
+use Illuminate\Support\Facades\Validator;
+
+use App\Http\Requests\HelloRequest;
+
 class HelloController extends Controller
 {
     public function index(Request $request) {
-        return view('hello.index', ['msg'=>'フォームを入力：']);
+        return view('hello.index', ['msg'=>'フォームを入力下さい。']);
     }
 
-    public function post(Request $request) {
-        $validate_rule = [
-            'name' => 'required',
-            'mail' => 'email',
-            'age' => 'numeric|between:0,150',
-        ];
-        $this->validate($request, $validate_rule);
+    public function post(HelloRequest $request) {
         return view('hello.index', ['msg'=>'正しく入力されました！']);
     }
 
