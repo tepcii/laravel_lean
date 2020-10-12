0cf23e6e24f0d694b4f01ab73c1bbeca27311c67
Tenma Endou <tenmaendou@MacBook-Air.local>
2020-10-12 21:16:07 +0900

    6-2 #12

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use App\Scopes\ScopePerson;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Person extends Model
 {
@@ -11,5 +13,27 @@ class Person extends Model
     public function getData()
     {
         return $this->id . ': ' . $this->name . ' （' . $this->age . '）';
+    }
+
+    public function scopeNameEqual($query, $str)
+    {
+        return $query->where('name', $str);
+    }
+
+    public function scopeAgeGreaterThan($query, $n)
+    {
+        return $query->where('age', '>=', $n);
+    }
+
+    public function scopeAgeLessThan($query, $n)
+    {
+        return $query->where('age', '<=', $n);
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new ScopePerson());
     }
 }
