ea9313657518d15d1ba974a4fe28b3078d769463
Tenma Endou <tenmaendou@MacBook-Air.local>
2020-10-13 23:10:22 +0900

    7-5 7-6 close #19 #20

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Person;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
@@ -28,6 +29,20 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+        ];
+    }
+}
+
+class PersonFactory extends Factory
+{
+    protected $model = Person::class;
+
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'mail' => $this->faker->safeEmail,
+            'age' => random_int(1,99),
         ];
     }
 }
---
+++ tests/Feature/HelloTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+
+class HelloTest extends TestCase
+{
+    use DatabaseMigrations;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+
+        $response = $this->get('/');
+        $response->assertStatus(200);
+
+        $response = $this->get('/hello');
+        $response->assertStatus(302);
+
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->get('/hello');
+        $response->assertStatus(200);
+
+        $response = $this->get('/no_route');
+        $response->assertStatus(404);
+    }
+}
