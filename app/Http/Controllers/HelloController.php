a7b8a5a36d2148a000675b2a67d5a857a5c07546
Tenma Endou <tenmaendou@MacBook-Air.local>
2020-10-13 22:05:27 +0900

    7-3 #17

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -9,12 +9,16 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
 
 use App\Http\Requests\HelloRequest;
+use App\Models\Person;
 
 class HelloController extends Controller
 {
     public function index(Request $request) {
-        $items = DB::table('people')->orderBy('age', 'asc')->get();
-        return view('hello.index', ['items' => $items]);
+        $sort = $request->sort;
+        // $items = DB::table('people')->orderBy($sort, 'asc')->simplePaginate(5);
+        $items = Person::orderBy($sort, 'asc')->paginate(5);
+        $param = ['items' => $items, 'sort' => $sort];
+        return view('hello.index', $param);
     }
 
     public function post(Request $request) {
